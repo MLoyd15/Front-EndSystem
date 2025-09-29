e1f5e847004e5ab82ef9d5964087e9df8a34ceb9
Loyd Marcelo <loydmarcelo15@gmail.com>
2025-09-29 22:11:03 +0800

    fix config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 import React from "react";
 
-export const API_BASE = "https://goat-agri-trading-backend.onrender.com/api";
-export const SOCKET_URL = "https://goat-agri-trading-backend.onrender.com";
+export const VITE_API_BASE = "https://goat-agri-trading-backend.onrender.com/api";
+export const VITE_SOCKET_URL = "https://goat-agri-trading-backend.onrender.com";
