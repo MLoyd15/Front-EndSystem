b0fb3150765e6520a31d1dbeedd84b3f66dacc6a
Loyd Marcelo <loydmarcelo15@gmail.com>
2025-09-29 22:25:58 +0800

    index

--- server/index.js
+++ server/index.js
@@ -33,6 +33,8 @@ app.use(
       "http://localhost:5173",   // Vite admin
       "http://127.0.0.1:5173",
       "http://localhost:19006",  // Expo web preview (if used)
+      "https://goat-agritrading-frontend.vercel.app",
+      
       
     ],
     credentials: true,
