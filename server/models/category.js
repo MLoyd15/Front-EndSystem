89a103a13cbadfdb81dbda4d9ad8a774bec9d966
Loyd Marcelo <loydmarcelo15@gmail.com>
2025-10-20 12:45:35 +0800

    fix category

--- server/models/category.js
+++ server/models/category.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
     categoryName: {type: String, required: true},
-    categoryDescription: {type: String, required: true}
+    categoryDescription: {type: String, required: false}
 })
 
 
