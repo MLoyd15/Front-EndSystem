b4422e462d1e850381b701c06e6bff18b9400628
Loyd Marcelo <loydmarcelo15@gmail.com>
2025-10-24 11:57:33 +0800

    fix chat

--- server/routes/supportChatRoutes.js
+++ server/routes/supportChatRoutes.js
@@ -8,7 +8,6 @@ import {
   getActiveChats,
   closeSupportChat
 } from '../controlers/supportChatControllers.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
